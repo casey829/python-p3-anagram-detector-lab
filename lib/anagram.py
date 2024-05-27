476b1b53cd526b40213d692b03bc2713ab8148ea
casey <caseymwangi31@gmail.com>
2024-05-27 10:19:53 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        #start with sorting the letters of the initial word
+        sorted_word = sorted(self.word)
+        matches = []
+
+        #iterate through the list of words
+        
+        for word in word_list:
+            #if the sorted words are the same, add to matches
+            if sorted(word) == sorted_word:
+                matches.append(word)
+                
+        return matches
